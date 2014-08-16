92049c980aadc93a309e28ca6705048e5301f669
vinjn <vinjn.z@gmail.com>
2014-08-16 12:54:09 +0800

    Update to 2d points and add GUI

--- NearestNeighbor/src/main.cpp
+++ NearestNeighbor/src/main.cpp
@@ -11,21 +11,54 @@ using namespace std;
 
 int main(int argc, char** argv )
 {
-    Mat1f inA(5, 1);
-    Mat1f inB(10, 1);
-    for (int i=0;i<5;i++) inA(i, 0) = rand() % 100;
-    for (int i=0;i<10;i++) inB(i, 0) = rand() % 100;
-    print(inA);printf("\n\n");
-    print(inB);printf("\n\n");
+    namedWindow("nearest");
+    const int W = 400;
+    const int H = 400;
+    const int NA = 20;
+    const int NB = 40;
+    Mat3b canvas(H, W);
+    Mat1f inA(NA, 2);
+    Mat1f inB(NB, 2);
 
-    Mat dist;
-    batchDistance(inA, inB, dist, CV_32FC1, noArray(), NORM_L2, 0, noArray(), 0, false);
-    print(dist);printf("\n\n");
+    while (true)
+    {
+        canvas.setTo(CV_BLACK);
 
-    Mat nidx;
-    batchDistance(inA, inB, dist, CV_32FC1, nidx, NORM_L2, 1, noArray(), 0, true);
-    print(dist);printf("\n\n");
-    print(nidx);printf("\n\n");
+        for (int i=0;i<NA;i++)
+        {
+            inA(i, 0) = rand() % W;
+            inA(i, 1) = rand() % H;
+            circle(canvas, Point(inA(i, 0), inA(i, 1)), 4, CV_RED);
+        }
+
+        for (int i=0;i<NB;i++)
+        {
+            inB(i, 0) = rand() % W;
+            inB(i, 1) = rand() % H;
+            circle(canvas, Point(inB(i, 0), inB(i, 1)), 4, CV_BLUE);
+        }
+        Mat dist;
+        Mat nidx;
+        batchDistance(inA, inB, dist, CV_32FC1, nidx, NORM_L2, 1, noArray(), 0, false);
+        print(dist);printf("\n\n");
+        print(nidx);printf("\n\n");
+
+        for (int i=0;i<NA;i++)
+        {
+            int nn = nidx.at<int>(i);
+            if (nn != -1)
+            {
+                line(canvas, Point(inA(i,0), inA(i, 1)),
+                    Point(inB(nn,0), inB(nn,1)),
+                    CV_WHITE);
+            }
+        }
+
+        imshow("nearest", canvas);
+
+        if (waitKey() == 0x1B)
+            break;
+    }
 
     return 0;
 }
